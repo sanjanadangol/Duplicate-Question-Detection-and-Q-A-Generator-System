b2c39edd4d1c7e26f96f6573a88762dcea0b918f
sanjanadangol <102642785+sanjanadangol@users.noreply.github.com>
2023-10-07 22:45:48 +0545

    Add files via upload

---
+++ QnA/app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import helper
+import pickle
+
+model = pickle.load(open('model.pkl','rb'))
+
+def redirect_button(url: str, text: str= None, color="#FD504D"):
+    st.markdown(
+    f"""
+    <a href="{url}" target="_self">
+        <div style="
+            display: inline-block;
+            padding: 0.5em 1em;
+            color: #FFFFFF;
+            background-color: {color};
+            border-radius: 3px;
+            text-decoration: none;">
+            {text}
+        </div>
+    </a>
+    """,
+    unsafe_allow_html=True
+    )
+
+st.header('Detection of Duplicate Questions')
+
+q1 = st.text_input('Enter question 1')
+q2 = st.text_input('Enter question 2')
+
+if st.button('Find'):
+    query = helper.query_point_creator(q1,q2)
+    result = model.predict(query)[0]
+
+    if result:
+        st.header('Duplicate')
+    else:
+        st.header('Not Duplicate')
+
+redirect_button("http://127.0.0.1:5001/","Click here to generate Q&A")
+
+
+
+
